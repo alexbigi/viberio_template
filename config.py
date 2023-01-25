249f6696168a601d40b5fd6d1970d8789d0c9118
Alex Dubovik <58629047+alexbigi@users.noreply.github.com>
2023-01-25 15:47:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-API_TOKEN = '4b52cbfd67a7d1c8-2af39938303798c9-4d3aec63c9228bc3'
+API_TOKEN = 'api token'
 WEBHOOK_URL = 'https://ea43-151-249-164-81.ngrok.io'
 BOT_NAME = 'ROBOT'
 AVATAR = 'https://cdn.icon-icons.com/icons2/1371/PNG/512/robot03_90833.png'
